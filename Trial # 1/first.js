b4438ece447b736d1085e6daaf6c8878e3f37040
emisner15 <em1909@humboldt.edu>
2018-01-20 13:49:41 -0800

    Saving and attempt to run cpp files

---
+++ Trial # 1/first.js
@@ -0,0 +1,33 @@
+
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+//create the server
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+
+      //choose the path for the files on the server here:
+      //relative paths
+      	
+      var newpath = './somefilefolder/' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+
+    //note: if there is an error in this section of code, it'll break the path on line 11.
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
