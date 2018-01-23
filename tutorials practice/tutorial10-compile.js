d7d4fcd8fa1f2875104c876e5fbbb2988e620d85
em1909 <em1909@gmail.com>
2018-01-23 14:01:07 -0800

    Fun with compiling

--- tutorials practice/tutorial10-compile.js
+++ tutorials practice/tutorial10-compile.js
@@ -1,17 +1,14 @@
-//is this right? is this how you define the file?
-var code = 'helloworld.cpp';
-var input ='./';
-
-
-
 var compile_run = require('compile-run');
-    compile_run.runCpp(code, input, function (stdout, stderr, err) {
+var path = require('path');
+
+
+    compile_run.runFile('C:/Program Files (x86)/Microsoft Visual Studio/2017/Enterprise/VC/Tools/MSVC/14.12.25827/bin/Hostx64/x64/cl.exe C:/Users/Eric/Desktop/acarteas-research/tutorials practice/helloworld.cpp',"",function (stdout, stderr, err) {
        if(!err){
-       console.log(stdout);
+        console.log(err);
         console.log(stderr);
+        console.log(stdout);
        }
        else{
-        console.log(err);
+        console.log(stdout,stderr);
        }
-    }
-);
+});
