a87c4c95e786e64e6d6743ebb4be328187ab8e9c
em1909 <em1909@gmail.com>
2018-01-25 10:06:46 -0800

    Tutorial 15 exec

--- tutorials practice/tutorial15-exec.js
+++ tutorials practice/tutorial15-exec.js
@@ -1,19 +1,14 @@
 var path = require('path');
-var path1 = 'C:/Program Files (x86)/Microsoft Visual Studio/2017/Enterprise/VC/Tools/MSVC/14.12.25827/bin/HostX64/x64/cl.exe';
+//var path1 = 'C:/Program Files (x86)/Microsoft Visual Studio/2017/Enterprise/VC/Tools/MSVC/14.12.25827/bin/HostX64/x64/cl.exe';
 var exec = require('child_process');
-var command = 'dir/w';
-
-	exec.execFile(path1,function(err,data){
+var command2 = '&&';
+var command = 'helloworld.cpp';
+var path2 ='C:/Users/Eric/Desktop/acarteas-research/bat_file'
+	exec.execFile(path2,function(err,data){
 		if (err){
 			console.log(err);	
 		}
-		exec.exec(command,function(err,data){
-			if (err){
-				console.log(err);
-			}
-			console.log(data);
-		});
-
+		command;
 		console.log(data);
 	});
 
