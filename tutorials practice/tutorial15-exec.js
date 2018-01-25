6e9a7dcd07835721fe78f5e83b15405bff564872
em1909 <em1909@gmail.com>
2018-01-25 09:29:37 -0800

    Updated tutorial15-exec.js

--- tutorials practice/tutorial15-exec.js
+++ tutorials practice/tutorial15-exec.js
@@ -1,13 +1,20 @@
 var path = require('path');
-var path1 = 'C:/Program Files (x86)/Microsoft Visual Studio/2017/Enterprise/VC/Tools/MSVC/14.11.25503/bin/HostX64/x64/cl.exe';
-var exec = require('child_process').execFile;
+var path1 = 'C:/Program Files (x86)/Microsoft Visual Studio/2017/Enterprise/VC/Tools/MSVC/14.12.25827/bin/HostX64/x64/cl.exe';
+var exec = require('child_process');
+var command = 'dir/w';
 
-var fun = function(){
-	console.log("fun() start");
-	exec(path1,function(err,data){
-		console.log(err);
+	exec.execFile(path1,function(err,data){
+		if (err){
+			console.log(err);	
+		}
+		exec.exec(command,function(err,data){
+			if (err){
+				console.log(err);
+			}
+			console.log(data);
+		});
+
 		console.log(data);
 	});
-}
 
-fun();
+
