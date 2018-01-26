4733d2694a9343d5fc13bdc0a834cee7219974fd
emisner15 <em1909@humboldt.edu>
2018-01-26 14:06:11 -0800

    more bats

---
+++ tutorials practice/compilejk.bat
@@ -0,0 +1,13 @@
+@REM soft coded example
+@REM SET vsCommand=%1
+@REM SET localPath=%2
+@REM SET cppFiles=%3
+@REM SET outputFileExe=%4
+@REM call %vsCommand%
+@REM cd /C %localPath%
+@REM cl %cppFiles% /Fe%outputFileExe
+
+@REM hard-coded example
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+CD "C:\Users\research\Desktop\acarteas-research\tutorials practice"
+cl.exe helloworld.cpp /Femain.exe
