42c7a5e8c850d4ae18cebf930a77ba83b350ff0f
bravelemming <bravelemming@gmail.com>
2018-02-12 17:06:28 -0800

    trial 5 is now dev env

---
+++ Trial5/compile.bat
@@ -0,0 +1,15 @@
+@REM soft coded example
+SET vsCommand=%1
+ECHO %vsCommand%
+SET localPath=%2
+ECHO %localPath%
+@REM SET cppFiles=%3
+@REM SET outputFileExe=%4
+@REM call %vsCommand%
+@REM cd /C %localPath%
+@REM cl %cppFiles% /Fe%outputFileExe
+
+@REM hard-coded example
+@REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+@REM CD "C:\Users\research\Desktop\acarteas-research\Trial 3"
+@REM cl.exe helloworld.cpp /Femain.exe
---
+++ Trial5/origin.js
@@ -0,0 +1,64 @@
+  var http = require('http');
+  var formidable = require('formidable');
+  var fs = require('fs');
+  var compile = require('./compile-module');
+  var runFile = require('./run-module');
+
+  //path variable for our executable files
+  var fileName = './main.exe';
+  //.cpp file folder location
+  var sourcefile = 'helloworld.cpp';
+
+  //create the server
+  http.createServer(function (req, res) {
+    if (req.url == '/fileupload') {
+      var form = new formidable.IncomingForm();
+      form.parse(req, function (err, fields, files) {
+        
+
+          //calling our compile module here
+          compile.compileFunction(sourcefile, fileName, runFile.runningExe );
+
+                  
+          //calling our run module here
+          //runFile.runningExe(fileName);
+
+        //var oldpath = files.filetoupload.path;
+
+        //choose the path for the files on the server here
+        //var newpath = './' + files.filetoupload.name;
+
+        /* this is for moving the file, which we are not doinng.
+        
+        //rename and move the file
+        fs.rename(oldpath, newpath, function (err) {
+          if (err) throw err;
+          res.write('File uploaded and moved!');
+		*/ //for moving the file
+
+
+  		//compiling
+          //var sourceCode;
+          //var readThis = './' + files.filetoupload.name;
+      	//read the file        
+          //fs.readFile( readThis , function(err, contents) {
+          //console.log(contents);
+          //sourceCode = contents;
+          
+          
+          return res.end();
+      //});  for moving the file
+
+        //});
+   });
+
+      //note: if there is an error in this section of code, it'll break the path on line 11.
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+      res.write('<input type="file" name="filetoupload"><br>');
+      res.write('<input type="submit">');
+      res.write('</form>');
+      return res.end();
+    }
+  }).listen(8080); //, '137.150.122.17'     
