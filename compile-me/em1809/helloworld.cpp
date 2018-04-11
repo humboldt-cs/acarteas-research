4d4b45e26b8e70bbbe44b2e3a4b1d081308342d9
emisner15 <em1909@humboldt.edu>
2018-04-11 15:34:37 -0700

    something

---
+++ compile-me/em1809/helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string sentence = "Hello,World"
+	cout << sentence <<endl;
+}
---
+++ source/em1802/compile.bat
@@ -0,0 +1,3 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+CD "C:\Users\research\Desktop\acarteas-research\source\em1802"
+cl.exe *.cpp /Femain.exe
